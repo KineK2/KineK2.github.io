994d84419769ab95a9c02a5a5965b6c0f20281ce
KineK2 <86829229+KineK2@users.noreply.github.com>
2021-07-03 13:22:16 +0700

    update index

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-login").addEventListener("click",()=>{
+    window.open("login.html")
+});
+document.getElementById("btn-login").addEventListener("click",()=>{
+    window.open("reg.html")
+});
+
+
+
